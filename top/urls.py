b98cbfaede9a969f915e409d721ac7c797b6f92f
flogtenno <flogtenno@gmail.com>
2024-03-13 15:20:18 +0900

    3.13 15:20 Tag追加・一覧表示機能追加

--- top/urls.py
+++ top/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.top,name="top"),
+    path('tag',views.tag,name="tag"),
+    path('tag_search/<int:tag_id>',views.tag_search,name="tag_search"),
     ]
--- top/views.py
+++ top/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render,redirect
-# from django.contrib.auth.models import User
 from django.contrib import messages #メッセージの送信完了をおしらせ
-# from django.core.paginator import Paginator #ページ区切り
+from django.core.paginator import Paginator #ページ区切り
 # from django.db.models import Q
-from django.contrib.auth.decorators import login_required
+from .forms import CreateTagForm
+from .models import Tag
 
-# from .models import Message,Good
-# from .forms import MessageForm
-
-# top（目次）___________________________________________________________________
+# トップページ（目次）___________________________________________________________________
 def top (request):
     params={
         "login_user"     :   request.user, #現在のログインユーザー情報（request.user）
@@ -19,3 +16,30 @@ def top (request):
     # messages.success(request, "test")
 
     return render(request, "top/top.html", params)
+
+# Tag追加________________________________________________________________________
+def tag(request):
+    form = CreateTagForm()
+    alltags = Tag.objects.all()
+    if request.method == 'POST':
+        form = CreateTagForm(request.POST)
+        if form.is_valid():
+            new_tag = form.cleaned_data['tag'] #フォーム内のtagを取り出して比較させる
+            if not Tag.objects.filter(tag=new_tag).exists(): #もし既存のタグと内容が同じならTrueなので、notで否定
+                form.save()
+                messages.success(request, f"「{new_tag}」タグを追加しました")
+            else:
+                messages.error(request, "このタグは既に存在します")
+            return redirect('tag')  # 成功した場合はリダイレクト
+    params={
+        "createform":   form,
+        "alltags"   :   alltags,
+    }
+    return render(request, 'top/tag.html', params)
+
+# Tag検索______________________________________________________________________
+
+def tag_search(request, tag_id):
+
+    pass
+
